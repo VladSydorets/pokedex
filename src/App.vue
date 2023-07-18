793bd9afa4de8d3aad84367fe4fcd0469ddce9ac
Vlad <sidorecvv@gmail.com>
2023-07-18 03:08:43 +0200

    create BackgroundPokemons component

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,11 @@
 import { RouterView } from 'vue-router'
 import PageHeader from './components/PageHeader.vue'
 import PageFooter from './components/PageFooter.vue'
-import { usePokemonsStore } from './stores/pokemons'
-// import type Pokemon from '@/stores/pokemons'
-
-// interface ImageObject {
-//   img: string
-//   name: string
-// }
-
-const pokemonsStore = usePokemonsStore()
-
-function getPokemonImage() {
-  const randomPokemon = pokemonsStore.findPokemon(pokemonsStore.getRandomPokemon())
-  return { img: randomPokemon['image-url'], name: randomPokemon.name }
-}
+import BackgroundPokemons from './components/BackgroundPokemons.vue'
 </script>
 
 <template>
-  <div class="first-img" v-if="pokemonsStore">
-    <img :src="getPokemonImage().img" :alt="getPokemonImage().name" />
-  </div>
-  <div class="second-img" v-if="pokemonsStore">
-    <img :src="getPokemonImage().img" :alt="getPokemonImage().name" />
-  </div>
+  <BackgroundPokemons />
   <PageHeader />
   <main>
     <RouterView />
@@ -35,19 +17,5 @@ function getPokemonImage() {
 <style scoped>
 .main {
   height: 100vh;
-}
-
-.first-img img {
-  position: absolute;
-  left: 0;
-  transform: translate(-40%, 30%);
-  rotate: -40deg;
-}
-
-.second-img img {
-  position: absolute;
-  right: 0;
-  transform: translate(80%, 90%);
-  rotate: 40deg;
 }
 </style>
---
+++ src/components/BackgroundPokemons.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { usePokemonsStore } from '@/stores/pokemons'
+// import type Pokemon from '@/stores/pokemons'
+
+// interface ImageObject {
+//   img: string
+//   name: string
+// }
+
+const pokemonsStore = usePokemonsStore()
+
+function getPokemonImage() {
+  const randomPokemon = pokemonsStore.findPokemon(pokemonsStore.getRandomPokemon())
+  return { img: randomPokemon['image-url'], name: randomPokemon.name }
+}
+</script>
+
+<template>
+  <div class="bg-pokemons">
+    <div class="first-img" v-if="pokemonsStore">
+      <img :src="getPokemonImage().img" :alt="getPokemonImage().name" />
+    </div>
+    <div class="second-img" v-if="pokemonsStore">
+      <img :src="getPokemonImage().img" :alt="getPokemonImage().name" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.first-img img {
+  position: absolute;
+  left: 0;
+  transform: translate(-40%, 30%);
+  rotate: -40deg;
+}
+
+.second-img img {
+  position: absolute;
+  right: 0;
+  transform: translate(80%, 90%);
+  rotate: 40deg;
+}
+</style>
